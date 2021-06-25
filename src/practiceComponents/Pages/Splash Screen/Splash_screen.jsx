31ea72d42a55aed405ac24c6f27d74b129dbdc2b
Sumit Kumar <Dy4366190@gmail.com>
2021-06-25 11:23:38 +0530

    updated

--- src/practiceComponents/Pages/Splash Screen/Splash_screen.jsx
+++ src/practiceComponents/Pages/Splash Screen/Splash_screen.jsx
@@ -5,7 +5,7 @@ class Splash_Screen extends Component {
         return ( 
             <div className="Splash_screen_container">
                 <lottie-player className="netflix_lottie" onClick={()=>{
-                    window.location.assign("http://localhost:3000/dashboard")
+                    window.location.assign("http://mflicks.herokuapp.com/dashboard")
                 }} src="https://assets3.lottiefiles.com/private_files/lf30_is6flrfu.json"  background="transparent"  speed="1"  style={{height : "100vh",width : "100%",backgroundColor : "black",cursor : "pointer"}}  autoplay/>
             </div>
          );
